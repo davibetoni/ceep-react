39a344c920eb90dea04381ad60d62f793ff6af6c
Davi Betoni <davibetoni@gmail.com>
2022-01-26 21:08:04 -0300

    usando state, props para a criacao de notas

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
-import './assets/index.css';
+import "./assets/index.css";
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notas:[]
+    };
+  }
+  createNote(title, text) {
+    const novaNota = { title, text };
+    const newNota = [...this.state.notas, novaNota]
+    const newState = {
+      notas:newNota
+    }
+    this.setState(newState);
+  }
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro createNote={this.createNote.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
--- src/components/FormularioCadastro/index.jsx
+++ src/components/FormularioCadastro/index.jsx
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
 import "./styles.css";
 class FormularioCadastro extends Component {
+  constructor(props) {
+    super(props);
+    this.titulo = "";
+    this.texto = "";
+  }
+
+  _handleChangeTitle(event) {
+    event.stopPropagation();
+    this.titulo = event.target.value;
+  }
+
+  _handleChangeText(event) {
+    event.stopPropagation();
+    this.texto = event.target.value;
+  }
+
+  _handleCreateNote(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    this.props.createNote(this.titulo, this.texto);
+  }
   render() {
     return (
-      <form className="form-cadastro ">
+      <form
+        className="form-cadastro"
+        onSubmit={this._handleCreateNote.bind(this)}
+      >
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this._handleChangeTitle.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._handleChangeText.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
